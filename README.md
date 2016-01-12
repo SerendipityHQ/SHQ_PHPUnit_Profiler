757d409d0ebef67a6b5d601257000901d87716e0
Aerendir <hello@aerendir.me>
2016-01-12 13:01:51 +0100

    Profile time took.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # SHQ_PHPUnit_Profiler
+
+    <listeners>
+        <listener class="SerendipityHQ\Library\PHPUnit_Profiler\Profiler">
+            <arguments>
+                <array>
+                    <element key="time"><boolean>true</boolean></element>
+                </array>
+            </arguments>
+        </listener>
+    </listeners>
--- src/Profiler.php
+++ src/Profiler.php
@@ -6,15 +6,35 @@ use Symfony\Component\Stopwatch\Stopwatch;
 
 class Profiler extends \PHPUnit_Framework_BaseTestListener
 {
-    private $stopwatch;
+    // Public options
+    private $profileTime = false;
 
     public function __construct($options = [])
     {
-        $this->stopwatch = new Stopwatch();
+        $this->initOptions($options);
     }
 
-    protected function getStopwatch()
+    public function startTest(\PHPUnit_Framework_Test $test)
     {
-        return $this->stopwatch;
+        // Start the Stopwatch
+        printf("\n\nStart test '%s'\n", $test->getName());
+    }
+
+    public function endTest(\PHPUnit_Framework_Test $test, $time)
+    {
+        printf("Ended test '%s'\n", $test->getName());
+
+        if ($this->profileTime) {
+            printf('[Time took] ');
+            printf("PHPUnit: %s ms ", round($time * 1000, 2));
+            printf("\n");
+        }
+    }
+
+    protected function initOptions(array $options)
+    {
+        if (isset($options['profileTime']) && is_bool($options['profileTime']))
+            $this->profileTime = $options['profileTime'];
+        }
     }
 }
