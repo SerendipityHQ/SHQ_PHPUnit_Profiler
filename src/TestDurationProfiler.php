daf3bab09ee6a5f3ac6b4306f83651bfac1bbfbe
Aerendir <hello@aerendir.me>
2016-01-12 11:04:38 +0100

    Add profilers.

---
+++ src/TestDurationProfiler.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @package     SHQ_PHPUnit_Profiler
+ *
+ * @author      Adamo Crespi <hello@aerendir.me>
+ * @copyright   Copyright (C) 2016.
+ * @license     MIT
+ */
+
+namespace SerendipityHQ\Library\PHPUnit_Profiler;
+
+use Symfony\Component\Stopwatch\Stopwatch;
+
+class TestDurationProfiler extends \PHPUnit_Framework_BaseTestListener
+{
+}
